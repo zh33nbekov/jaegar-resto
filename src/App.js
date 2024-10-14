6772609a126839ff873e5c53950435f9e8f6d8d4
Rai <Rai@MacBook-Pro.local>
2024-10-15 00:24:06 +0600

    change com

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import Backdrop from './components/UI/backdrop/Backdrop';
 import Preloader from './components/UI/preloader/Preloader';
-import Orders from './components/orders/Orders';
 import DishBasketContextProvider from './context/dishes/DishBasketContext';
 import DishesContextProvider from './context/dishes/DishesContext';
 import NavigationContextProvider from './context/navigation/NavigationContext';
@@ -12,7 +11,6 @@ const AppContent = () => {
 		<div>
 			<Backdrop />
 			<Preloader />
-			<Orders />
 		</div>
 	);
 };
