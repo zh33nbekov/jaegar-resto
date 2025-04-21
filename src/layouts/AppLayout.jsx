eb80ece6716d26659714ebc36ffb092ab7940e80
zh33nbekov <rai.zheenbekov@gmail.com>
2025-04-21 19:29:07 +0600

    feat: implement Footer component with intersection observer for dynamic rendering

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useIntersection } from 'react-use'
+import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 
 const styles = {
@@ -6,12 +8,34 @@ const styles = {
 	height: '100%',
 	display: 'flex',
 }
+const lineStyles = {
+	border: 'none',
+}
+
 const AppLayout = ({ children }) => {
+	const [isIntersecting, setIsIntersecting] = useState(false)
+	const footerTriggerRef = useRef(null)
+	const intersection = useIntersection(footerTriggerRef, {})
+
+	useEffect(() => {
+		if (intersection?.isIntersecting) {
+			setIsIntersecting(true)
+		} else {
+			setIsIntersecting(false)
+		}
+	}, [intersection?.isIntersecting])
+
 	return (
-		<div style={styles}>
-			<Navbar />
-			<main className='wrapper'>{children}</main>
-		</div>
+		<>
+			<div style={styles}>
+				<Navbar />
+				<main className='wrapper'>
+					{children}
+					<hr ref={footerTriggerRef} style={lineStyles} />
+					{isIntersecting && <Footer />}
+				</main>
+			</div>
+		</>
 	)
 }
 
