855511960f7b040cc270b37253f2f636071892a6
zh33nbekov <rai.zheenbekov@gmail.com>
2025-04-21 17:48:15 +0600

    feat: add Footer component with initial content and styles

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx'
+import styles from './footer.module.css'
+
+const Footer = () => {
+	return (
+		<footer className={clsx(styles.footer, 'wrapper')}>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+			reprehenderit. Molestias numquam temporibus voluptatum tempora
+			voluptatem consequuntur aliquam, illum vero consequatur repellat autem
+			exercitationem totam eum beatae tempore illo assumenda atque ad! Quidem
+			veritatis perspiciatis soluta cupiditate asperiores. Ducimus
+			perspiciatis esse totam explicabo excepturi voluptate debitis. Eligendi
+			ad officiis iusto in blanditiis atque error sed consectetur deserunt
+			mollitia, inventore tempore possimus! Magni odio ex fugiat laborum
+			quibusdam maiores accusamus deleniti quidem. Labore modi quisquam
+			deserunt, veniam quos maxime minus, non doloribus earum corrupti
+			exercitationem, esse quam cumque aspernatur error fugiat itaque
+			dolorum! Doloremque nam minima non. Vero, consectetur expedita,
+			officiis qui nam nemo aliquam soluta voluptatem unde tenetur hic!
+			Voluptatum, similique!
+		</footer>
+	)
+}
+
+export default Footer
