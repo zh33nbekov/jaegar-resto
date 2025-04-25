d4a5fc6cba36cdaee1c00b74a50682cb4c625d9a
zh33nbekov <rai.zheenbekov@gmail.com>
2025-04-25 23:10:31 +0600

    feat: implement search term state management with localStorage in HeaderSearch component

--- src/components/header-search/HeaderSearch.jsx
+++ src/components/header-search/HeaderSearch.jsx
@@ -1,9 +1,14 @@
-import { memo, useRef } from 'react'
+import { memo, useRef, useState } from 'react'
 import { Search } from '../../assets/icons/common/Search'
 import styles from './header-search.module.css'
 
 export const HeaderSearch = memo((props) => {
+	const [searchTerm, setSearchTerm] = useState(() => {
+		const storageSearchTerm = localStorage.getItem('searchTerm')
+		return storageSearchTerm
+	})
 	const searchRef = useRef(null)
+
 	return (
 		<div className={styles.search}>
 			<button className={styles.search__button}>
@@ -12,8 +17,13 @@ export const HeaderSearch = memo((props) => {
 			<input
 				type='search'
 				ref={searchRef}
-				className={styles.search__input}
+				value={searchTerm}
 				placeholder={props.placeholder}
+				className={styles.search__input}
+				onChange={(e) => {
+					setSearchTerm(e.target.value)
+					localStorage.setItem('searchTerm', e.target.value)
+				}}
 			/>
 		</div>
 	)
